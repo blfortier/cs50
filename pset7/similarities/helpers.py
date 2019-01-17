45540d1a1ff0478194def155e705fb6b772d8425
Brittney <brittfort34@gmail.com>
2019-01-17 20:51:54 +0000

    Updated helper functions

--- pset7/similarities/helpers.py
+++ pset7/similarities/helpers.py
@@ -1,19 +1,42 @@
+from nltk.tokenize import sent_tokenize
+
+
 def lines(a, b):
     """Return lines in both a and b"""
 
-    # TODO
-    return []
+    set_a = set(a.splitlines())
+    set_b = set(b.splitlines())
 
+    return set_a & set_b
 
 def sentences(a, b):
     """Return sentences in both a and b"""
 
-    # TODO
-    return []
+    set_a = set(sent_tokenize(a))
+    set_b = set(sent_tokenize(b))
 
+    return set_a & set_b
 
 def substrings(a, b, n):
     """Return substrings of length n in both a and b"""
 
-    # TODO
-    return []
+    subs = []
+
+  #  for word in range(len(a)):
+  #     subs_in_a = set(a[word:n])
+
+    #for words in range(len(b)):
+     #   print(words)
+      #  subs_in_b = set(b[words:n])
+
+    #print(subs_in_a)
+    #print(subs_in_b)
+
+    #subs = list(subs_in_a & subs_in_b)
+
+    #print(subs)
+
+
+     # TODO
+    return subs
+
--- pset7/similarities/test.py
+++ pset7/similarities/test.py
@@ -1,12 +1,15 @@
 from cs50 import get_string
 from sys import argv
 
+string_1 = "To love, or to have loved,this suffices. Demand nothing more. There is no other pearl to be found in the shadowy folds of life."
 
-set_1 = set(open(argv[1]).read().split())
+string_2 = "To love or have loved, that is enough. Ask nothing further. There is no other pearl to be found in the dark folds of life."
 
-set_2 = set(open(argv[2]).read().split())
+set_1 = set(string_1.split())
 
-print("set_1: {}".format(set_1))
-print("set_2: {}".format(set_2))
+set_2 = set(string_2.split())
 
-print("common: {}".format(set_1 & set_2))
+common = list(set_1 & set_2)
+
+
+print("results: {}".format(common))
